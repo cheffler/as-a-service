76444e8e60b093f591cdf3ad611672df2a0fd104
James Leffler <james.leffler@constelcom.com>
2021-05-20 16:40:11 +0100

    base ui

---
+++ ui-base/src/index.js
@@ -0,0 +1,19 @@
+(function(){
+  console.log('Hello, I will do something useful soon')
+
+  // document.addEventListener('DOMContentLoaded', function(){
+  //   var baseDiv = document.getElementById('base');
+  //   console.log('Got the base element', baseDiv)
+
+  //   const xhttp = new XMLHttpRequest();
+
+  //   xhttp.onreadystatechange = function(){
+  //     if (this.readyState ==4 && this.status == 200){
+  //       baseDiv.innerHTML = this.responseText;
+  //     }
+  //   }
+
+  //   xhttp.open('GET', '', true);
+  //   xhttp.send();
+  // })
+})()
